848b3e995f8fb27a058eb5814c6fbfd927560528
Chantelsky <laver.chantelsky@gmail.com>
2021-08-07 15:42:25 +1000

    abstract classes, implemented linkedlist

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
-import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+const numbersCollection = new NumbersCollection([50, 3, -5, 0]);
+numbersCollection.sort();
 console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+charactersCollection.sort();
+console.log(charactersCollection.data);
